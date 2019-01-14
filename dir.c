9ea3056b2ea192315f1cba2109e3e2481479bfc6
Luke Gruber <luke.gru@gmail.com>
2019-01-13 21:59:51 -0500

    add some more Dir methods

--- dir.c
+++ dir.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <dirent.h>
 #include "object.h"
 #include "vm.h"
 #include "runtime.h"
@@ -8,6 +9,78 @@
 #include "memory.h"
 
 ObjClass *lxDirClass = NULL;
+
+typedef struct LxDir {
+    DIR *dir;
+    bool open;
+} LxDir;
+
+static inline LxDir *dirGetHidden(Value dir) {
+    return (LxDir*)AS_INSTANCE(dir)->internal->data;
+}
+
+static void freeInternalDir(Obj *internalObj) {
+    ASSERT(internalObj->type == OBJ_T_INTERNAL);
+    ObjInternal *internal = (ObjInternal*)internalObj;
+    LxDir *ldir = (LxDir*)internal->data;
+    if (ldir->open) {
+        closedir(ldir->dir);
+    }
+}
+
+static Value lxDirInit(int argCount, Value *args) {
+    CHECK_ARITY("Dir#init", 2, 2, argCount);
+    ObjInstance *selfObj = AS_INSTANCE(*args);
+    Value name = args[1];
+    CHECK_ARG_IS_A(name, lxStringClass, 1);
+    int last = errno;
+    const char *dirStr = AS_STRING(name)->chars;
+    DIR *d = opendir(dirStr);
+    if (!d) {
+        int err = errno;
+        errno = last;
+        throwArgErrorFmt("Given directory '%s' could not be opened: %s",
+                dirStr, strerror(err));
+    }
+    ObjInternal *internalObj = newInternalObject(false, NULL, sizeof(LxDir), NULL, freeInternalDir, NEWOBJ_FLAG_NONE);
+    selfObj->internal = internalObj;
+    LxDir *ldir = ALLOCATE(LxDir, 1);
+    ldir->dir = d;
+    ldir->open = true;
+    internalObj->data = ldir;
+    return *args;
+}
+
+static Value lxDirIterNext(int argCount, Value *args) {
+    LxDir *dir = dirGetHidden(*args);
+    struct dirent *de;
+    if ((de = readdir(dir->dir))) {
+        return OBJ_VAL(copyString(de->d_name, strlen(de->d_name), NEWOBJ_FLAG_NONE));
+    } else {
+        return NIL_VAL;
+    }
+}
+
+static Value lxDirClose(int argCount, Value *args) {
+    CHECK_ARITY("Dir#close", 1, 1, argCount);
+    LxDir *ldir = dirGetHidden(*args);
+    if (!ldir->open) {
+        return FALSE_VAL;
+    }
+    int res = closedir(ldir->dir); // TODO: Should we throw here?
+    ldir->open = false;
+    return BOOL_VAL(res == 0);
+}
+
+static Value lxDirRewind(int argCount, Value *args) {
+    CHECK_ARITY("Dir#rewind", 1, 1, argCount);
+    LxDir *ldir = dirGetHidden(*args);
+    if (!ldir->open) {
+        return FALSE_VAL; // TODO: should we throw here?
+    }
+    rewinddir(ldir->dir);
+    return TRUE_VAL;
+}
 
 static Value lxDirPwdStatic(int argCount, Value *args) {
     CHECK_ARITY("Dir.pwd", 1, 1, argCount);
@@ -62,6 +135,10 @@ static Value lxDirChdirStatic(int argCount, Value *args) {
 
 void Init_DirClass() {
     lxDirClass = addGlobalClass("Dir", lxObjClass);
+    addNativeMethod(lxDirClass, "init", lxDirInit);
+    addNativeMethod(lxDirClass, "close", lxDirClose);
+    addNativeMethod(lxDirClass, "rewind", lxDirRewind);
+    addNativeMethod(lxDirClass, "iterNext", lxDirIterNext);
     ObjClass *dirStatic = classSingletonClass(lxDirClass);
     addNativeMethod(dirStatic, "pwd", lxDirPwdStatic);
     addNativeMethod(dirStatic, "chdir", lxDirChdirStatic);
