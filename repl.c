c573c6fc53c013f8fe62d89a63224b9831c8e2e0
Luke Gruber <luke.gru@gmail.com>
2019-01-29 10:32:14 -0500

    allow repl to continue after uncaught error

--- repl.c
+++ repl.c
@@ -21,6 +21,7 @@ static bool evalLines(char *lines[], int numLines) {
     resetStack();
     _freeChunk();
     vm.exited = false;
+    THREAD()->hadError = false;
     ObjString *buf = hiddenString("", 0, NEWOBJ_FLAG_NONE);
     for (int i = 0; i < numLines; i++) {
         char *line = lines[i];
